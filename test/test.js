24324e00d3909996876d5badd3ea9c62fecfa65c
Jeremial <jeremial90@gmail.com>
2013-12-05 16:47:29 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ describe('one', function(){
     it('should return 1', function(){
         sinon.spy(myObj, 'return1');
         expect(myObj.return1()).to.equal(1);
-        expect(myObj.return1.calledOnce).to.be.ok;
+        myObj.return1.should.have.been.calledOnce; // should usage
+        expect(myObj.return1.calledOnce).to.be.ok; // expect usage
     })
 
     it('should return 2', function(){
