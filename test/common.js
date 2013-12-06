6d2aacd54ea2c1189282d2789b638fa9a7d23230
Jeremial <jeremial90@gmail.com>
2013-12-06 15:11:31 +0800

    add node test example

---
+++ test/common.js
@@ -0,0 +1,9 @@
+"use strict";
+global.chai      = require('chai');
+global.sinon     = require('sinon');
+global.expect    = chai.expect;
+global.should    = chai.should();
+global.MVCObject = require('../MVCObject');
+
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
