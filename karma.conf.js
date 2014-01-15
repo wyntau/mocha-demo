70cac57ebb286678b70d485f5ab96cea854a58c8
Jeremial <Jeremial90@gmail.com>
2014-01-15 16:22:59 +0800

    update demo

--- karma.conf.js
+++ karma.conf.js
@@ -23,8 +23,7 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-    'test/common.js',
-    'test/mocha.opts'
+    'test/common.js'
     ],
 
 
--- test/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-global.chai      = require('chai');
-global.sinon     = require('sinon');
-global.expect    = chai.expect;
-global.should    = chai.should();
-global.MVCObject = require('mvcobject');
-
-var sinonChai = require('sinon-chai');
-chai.use(sinonChai);
